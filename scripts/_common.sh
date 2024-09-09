9c99904eda6839c3610d3a4f46497d7ce1027662
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-09 21:50:25 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,13 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+_create_data_dirs_if_required() {
+    dirs=(_data galleries upload)
+    for dir in "${dirs[@]}"; do
+        if [ ! -d "$data_dir/$dir" ]; then
+            rsync -a "$install_dir/$dir/" "$data_dir/$dir/"
+        fi
+        ynh_safe_rm "$install_dir/$dir"
+    done
+}
